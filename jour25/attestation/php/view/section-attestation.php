a0ff2b852415fa8f605eb575e4912d456318ff90
lordmanfr <husseinmahmoudfr@gmail.com>
2020-03-18 12:08:09 +0100

    page verifie

--- jour25/attestation/php/view/section-attestation.php
+++ jour25/attestation/php/view/section-attestation.php
@@ -1,4 +1,5 @@
-<section>
+
+        <section>
             <h2>FORMULAIRE DE DECLARATION POUR ATTESTATION</h2>
             <form action="" method="POST">
                 <input type="text" name="nom" required placeholder="votre nom">
@@ -44,22 +45,23 @@
                 <!-- ce sera utile avec JS et ajax pour sélectionner la balise... -->
                 <div class="confirmation">
 <?php
+// CODE POUR TRAITER LE FORMULAIRE
 
-{
-    // SECURITE: ?? "" => SI LE NAVIGATEUR N'ENVOIE PAS L'INFO, ON PREND COMME VALEUR PAR DEFAUT ""
-    $info = $_REQUEST[$name] ?? "";
-    // ON POURRA RAJOUTER PLUS DE SECURITE
-    // ...
-
-    return $info;
-}
+// ON SEPARE LE CODE DE NOS FONCTIONS DANS UN FICHIER COMMUN
+require_once "php/mes-fonctions.php";
 
 
+// ATTENTION: LE CODE PHP EST ASSOCIE AU CODE HTML
+// <input type="hidden" name="identifiantFormulaire" value="declaration">
+
+// VERIFIER SI LE FORMULAIRE A ETE ENVOYE
+// $identifiantFormulaire = $_REQUEST["identifiantFormulaire"] ?? "";
 $identifiantFormulaire = filtrer("identifiantFormulaire");
 
 if ($identifiantFormulaire == "declaration")
 {
 
+    // ALORS JE VAIS RECUPERER LES INFOS
     $tabAssoColonneValeur = [
 //        "nom"     => $_REQUEST["nom"] ?? "",
         "nom"       => filtrer("nom"),
@@ -75,34 +77,20 @@ if ($identifiantFormulaire == "declaration")
          && $adresse != ""
          && $raison != "")
          {
-       
+            // ENREGISTRER LES INFOS DANS LA TABLE SQL
+            // IL MANQUE DES INFOS POUR LA TABLE SQL
+            // IL MANQUE numero et dateDeclaration
+            // => IL FAUT COMPLETER LES INFOS MANQUANTES
+            // https://www.php.net/manual/fr/function.uniqid.php
+            // => PHP VA CREER UNE COMBINAISON ALEATOIRE DE CHIFFRES ET DE LETTRES
             $tabAssoColonneValeur["numero"] = uniqid();
             // https://www.php.net/manual/fr/function.date.php
             $tabAssoColonneValeur["dateDeclaration"] = date("Y-m-d H:i:s");
 
-            // MAINTENANT JE PEUX CONSTRUIRE LA REQUETE SQL PREPAREE
-            $requeteSQL =
-<<<CODESQL
-
-INSERT INTO declaration
-(nom, prenom, adresse, raison, numero, dateDeclaration) 
-VALUES 
-(:nom, :prenom, :adresse, :raison, :numero, :dateDeclaration) 
-
-CODESQL;
-            // ENSUITE, ON VA ENVOYER LA REQUETE SQL PREPAREE
-            // CONNECTER A SQL
-            
-            // ETAPE1: CONNECTER PHP A SQL
-            // ATTENTION: NE PAS OUBLIER DE CHANGER LA DATABASE...
-            $pdo = new PDO("mysql:host=localhost;dbname=attestation;charset=utf8;", "root", "");
-
-            // ETAPE2a: ON ENVOIE LA REQUETE PREPAREE
-            // PDOStatement EST UN CONTAINER QUI ENGLOBE LES RESULTATS DE LA REQUETE SQL
-            $pdoStatement = $pdo->prepare($requeteSQL);
-
-            // ETAPE2b: ON FOURNIT LES DONNEES EXTERIEURES A PART
-            $pdoStatement->execute($tabAssoColonneValeur);
+            // ETAPE2: ACTIVER LE CODE DE LA FONCTION
+            insererLigneSQL("declaration", $tabAssoColonneValeur);
+            // PHP FAIT $nomTable = "declaration" 
+            // PHP FAIT $param1 = $tabAssoColonneValeur
 
             // DEBUG
             echo "votre déclaration est bien enregistrée. NOTEZ BIEN VOTRE NUMERO D'ATTESTATION {$tabAssoColonneValeur["numero"]}";
