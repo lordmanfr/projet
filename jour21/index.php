d4a04df2ef2cd989e2fb5ca571d078b33995e6d6
lordmanfr <husseinmahmoudfr@gmail.com>
2020-03-23 15:57:33 +0100

    cree

--- jour21/index.php
+++ jour21/index.php
@@ -1,4 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <section>
+    <table width=100%>
+ <?php
 function additionner ($tabNombre)
 {
     // DEBUG
@@ -173,7 +184,7 @@ echo "<h1>PRIX TOTAL PANIER: $prixTotal euros</h1>";
 
 // EXO 11
 
-function creerInsertSQL ($nomTable, $tabAssoColVal)
+function creerInsertSQL1 ($nomTable, $tabAssoColVal)
 {
     // ON DOIT CONCATENER LES INFORMATIONS RECUES EN PARAMETRE
     // POUR OBTENIR LE CODE SQL ATTENDU
@@ -226,7 +237,7 @@ CODESQL;
     return $resultat;
 }
 
-$requeteSQLPreparee = creerInsertSQL(
+$requeteSQLPreparee = creerInsertSQL1(
     "newsletter", 
     [ "nom" => "julie", "email" => "julie@nomail.me" ]);
 
@@ -282,5 +293,212 @@ $codeSQL = creerDeleteSQL("contact", 5);
 echo "<h1>LE CODE SQL EST: $codeSQL</h1>";
 
 
+echo "<h1>--- EXO 11</h1>";
 
+function creerInsertSQL2 ($nomTable, $tabAssoColVal)
+{
+    // ON DOIT CONCATENER LES INFORMATIONS RECUES EN PARAMETRE
+    // POUR OBTENIR LE CODE SQL ATTENDU
+
+    // VALEURS INITIALES
+    $liste1 = "";
+    $liste2 = "";
+
+    // JE VEUX EXTRAIRE LES CLES DU TABLEAU ASSOCIATIF
+    // => BOUCLE
+    // ATTENTION: JE SUIS OBLIGE DE CREER LA VARIABLE $valeur 
+    // MEME SI ELLE NE SERT PAS
+
+    // ASTUCE: SI J'AI BESOIN DE L'INDICE ALORS JE LE RAJOUTE EN PLUS 
+    $indice = 0;
+    foreach($tabAssoColVal as $cle => $valeur)
+    {
+        // echo "(DEBUG:$cle/$valeur)";
+
+        // ASTUCE: ON UTILISE $indice POUR SAVOIR SI ON EST SUR LE PREMIER ELEMENT
+        if ($indice == 0)
+        {
+            // PREMIER ELEMENT: PAS DE VIRGULE
+            $liste1 = $liste1 . $cle;
+            $liste2 = $liste2 . ":". $cle;
+        }
+        else
+        {
+            // LES ELEMENTS APRES: AVEC VIRGULE
+            $liste1 = $liste1 . ", " . $cle;
+            $liste2 = $liste2 . ", :". $cle;
+
+        }
+
+        // IL NE FAUT PAS OUBLIER DE L'INCREMENTER
+        $indice++;
+    }
+
+    $resultat = 
+<<<CODESQL
+
+INSERT INTO $nomTable
+( $liste1 )
+VALUES
+( $liste2 )
+
+CODESQL;
+
+
+    // ON DOIT RENVOYER UN TEXTE QUI EST UN CODE SQL
+    return $resultat;
+}
+
+$requeteSQLPreparee = creerInsertSQL2(
+    "newsletter", 
+    [ "nom" => "julie", "email" => "julie@nomail.me" ]);
+
+echo "<pre>$requeteSQLPreparee</pre>";
+
+
+
+echo "<h1>--- EXO 12</h1>";
+
+/*
+ON DEVRAIT OBTENIR LE TEXTE CONCATENE
+
+INSERT INTO newsletter
+( nom, email )
+VALUES
+( :nom, :email )
+
+*/
+/*exo12: CREER UNE FONCTION creerUpdateSQL 
+    LA FONCTION PREND 3 PARAMETRES
+    LE NOM DE LA TABLE SQL:     $nomTable
+    UN TABLEAU ASSOCIATIF:      $tabAssoColVal
+
+    EXEMPLE D'UTILISATION:
+
+    $requeteSQLPreparee = creerUpdateSQL(
+                            "newsletter", 
+                            [ "nom" => "julie", "email" => "julie@nomail.me" ]);
+
+    echo "<pre>$requeteSQLPreparee</pre>";
+
+    
+    ON DEVRAIT OBTENIR LE TEXTE CONCATENE
+
+    UPDATE newsletter
+    SET
+    nom = :nom,
+    email = :email
+    WHERE 
+    id = :id
+
+ 
+    // ATTENTION: 
+    // LES VALEURS DU TABLEAU ASSOCIATIF NE SONT PAS UTILISEES
+    // ON MET DES JETONS (TOKENS) A LA PLACE DES VALEURS
+    // SEULS LES CLES DU TABLEAU ASSOCIATIF SERVENT...
+
+    */
+  
+    echo "<h1>--- EXO 13</h1>";  
+
+$ligne=4;
+$colonne=4;
+  for ($i=0;$i<$ligne; $i++){
+     for ($j=0;$j<$colonne; $j++){
+      if(($j+$i)%2==0)
+echo 'X';
+     else
+echo '0';
+    }
+    echo '<br>';
+  }
+  echo "<h1>--- EXO 13</h1>"; 
+
+ 
+ 
+  $ligne=8;
+  $colonne=8;
+    for ($i=0;$i<$ligne; $i++){
+        echo'<tr>';
+       for ($j=0;$j<$colonne; $j++){
+        if(($j+$i)%2==0)
+  echo '<td>&nbsp</td>';
+       else
+  echo '<td bgcolor=red>&nbsp</td>';
+      }
+      echo '</tr>';
+    }
+   echo "<h1>test</h1>"
+
+
+
+   /* function calculerPrixTotal1 ($tabQuantite1, $tabPrixUnitaire)
+{
+    // ICI IL FAUT AJOUTER LE CODE MANQUANT
+    // PRIXTOTAL = 1x10 + 2x20 + 3x30 + 4x40
+    // J'AI PARCOURU LES 2 TABLEAUX
+    // POUR PRENDRE CHAQUE ELEMENT
+    // ET JE MULTIPLIE LES VALEURS => SOUS-TOTAL
+    // ET JE RAJOUTE LE SOUS-TOTAL AU TOTAL
+    
+    // VALEUR INITIALE
+    $resultat = 0;
+
+    // J'AI PARCOURU LES 2 TABLEAUX
+    // BOUCLE => for
+    for ($indice=0; $indice < count($tabPrixUnitaire); $indice++)
+    {
+        // POUR PRENDRE CHAQUE ELEMENT
+        // ET JE MULTIPLIE LES VALEURS => SOUS-TOTAL
+        $prixUnitaire = $tabPrixUnitaire[$indice];
+        $quantite     = $tabQuantite1[$indice];
+        $sousTotal    = $prixUnitaire * $quantite;
+
+        // ET JE RAJOUTE LE SOUS-TOTAL AU TOTAL
+        $total = $resultat + $sousTotal;    
+    }
+
+    return $total;
+}
+
+
+$tabQuantite1     = [ 1,  2,  3,  4 ];
+$tabPrixUnitaire = [ 5, 10, 20, 50, 100, 200, 500 ];
+$prixTotal       = calculerPrixTotal($tabQuantite1, $tabPrixUnitaire);    
+// $texte = distribuerBillets(240);
+
+echo "<h1>PRIX TOTAL PANIER: $prixTotal euros</h1>";
+
+
+
+
+
+   /* ON CONSIDERE QU'ON A DES BILLETS DE 5, 10, 20, 50, 100, 200, 500 euros
+
+// CREER UNE FONCTION QUI PREND UN MONTANT EN PARAMETRE
+// ET QUI DISTRIBUE LE BON NOMBRE DE BILLETS
+// (ON DISTRIBUE LE MOINS DE BILLETS POSSIBLE...)
+
+function distribuerBillets ($montant)
+{
+    // AJOUTER LE CODE DE LA FONCTION...
+    return $resultat;
+}
+
+$texte = distribuerBillets(240);
+// $texte = "1x200 2x20";
+
+$texte = distribuerBillets(670);
+// $texte = "1x500 1x100 1x50 1x20";
+*/
+  
+  ?>
+
+
+  </table>
+
+  
+</html>
+
+ 
 
