600fdd09b4cbf17970fa8b432c72fdefc329362e
lordmanfr <husseinmahmoudfr@gmail.com>
2020-04-01 13:15:34 +0200

    new

---
+++ jour36/projet todolist/api/config.php
@@ -0,0 +1,14 @@
+<?php
+const DSN = 'mysql:host=localhost;dbname=todolist;charset=utf8' ;
+
+const USERNAME ="root" ;
+
+const PDW = "";
+
+
+
+const OPTIONS = [
+    PDO :: ATTR_DEFAULT_FETCH_MODE => PDO :: FETCH_ASSOC,
+
+    PDO :: ATTR_ERRMODE => PDO :: ERRMODE_EXCEPTION,
+];
---
+++ jour36/projet todolist/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <h1>Todolist Ajax</h1>
+    <form action="" method="post">
+        <div>
+            <label for="title">la tâche</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <input type="text" name="description" id="description">
+        </div>
+        <button>Save it</button>
+    </form>
+    <div class="todos"></div>
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
